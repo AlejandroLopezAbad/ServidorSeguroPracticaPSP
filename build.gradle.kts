7abf5f8d6fdf17f5beaf3491be85f73294ccfc6a
Alejandro <alexlopezabad13@gmail.com>
2023-01-16 19:43:26 +0100

    Preparacion Practica

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
     application
 }
 
@@ -14,6 +15,18 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // Bcrypt
+    implementation("at.favre.lib:bcrypt:0.9.0")
+    //implementation("de.nycode:bcrypt:2.2.0")
+    // Logger
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+    // Serializacion Json
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    // Corrutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    //JWT
+    implementation("com.auth0:java-jwt:4.2.1")
 }
 
 tasks.test {
